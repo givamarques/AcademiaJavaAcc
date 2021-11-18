874ceef04f4e3b99ffd17bd84e29c91f67eb5f86
Mateus Gomes <mateus10gomes@gmail.com>
2021-11-18 11:27:00 -0300

    Supervisor GET, POST, DEL
    OK

--- machine.io/src/main/java/io/machine/machine/io/controller/SupervisorController.java
+++ machine.io/src/main/java/io/machine/machine/io/controller/SupervisorController.java
@@ -1,21 +1,50 @@
 package io.machine.machine.io.controller;
 
+import io.machine.machine.io.model.Operador;
 import io.machine.machine.io.model.Supervisor;
 import io.machine.machine.io.service.SupervisorService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "/supervisor")
 public class SupervisorController {
 
     @Autowired
     private SupervisorService supervisorService;
+    private ModelMapper modelMapper;
 
-    @RequestMapping(value = "/supervisores")
-    private List<Supervisor> getAllSupervisor() {
-        return supervisorService.getAllSupervisor();
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Supervisor salvar(@RequestBody Supervisor supervisor){
+        return supervisorService.save(supervisor);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Supervisor> SupervisorList(){
+        return supervisorService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Supervisor SupervisorById(@PathVariable("id") Long id){
+        return supervisorService.buscarById(id).
+                orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Supervisor não encontrada"));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removerSupervisor(@PathVariable("id") Long id){
+        supervisorService.buscarById(id)
+                .map(supervisor -> {
+                    supervisorService.removeById(supervisor.getIdSupervisor());
+                    return Void.TYPE;
+                }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Maquina não encontrada"));
     }
 }
--- machine.io/src/main/java/io/machine/machine/io/service/SupervisorService.java
+++ machine.io/src/main/java/io/machine/machine/io/service/SupervisorService.java
@@ -1,5 +1,6 @@
 package io.machine.machine.io.service;
 
+import io.machine.machine.io.model.Operador;
 import io.machine.machine.io.model.Supervisor;
 import io.machine.machine.io.repository.SupervisorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,16 +8,27 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SupervisorService {
 
     @Autowired
     SupervisorRepository supervisorRepository;
-
-    public List<Supervisor> getAllSupervisor() {
-        List<Supervisor> supervisor = new ArrayList<Supervisor>();
-        supervisorRepository.findAll().forEach(supervisor::add);
-        return supervisor;
+    public List<Supervisor> findAll() {
+        return (List<Supervisor>) supervisorRepository.findAll();
     }
+
+    public Optional<Supervisor> buscarById(Long id){
+        return supervisorRepository.findById(id);
+    }
+
+    public Supervisor save(Supervisor supervisor){
+        return supervisorRepository.save(supervisor);
+    }
+
+    public void removeById(Long id){
+        supervisorRepository.deleteById(id);
+    }
+
 }
